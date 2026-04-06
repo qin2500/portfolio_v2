554e413dd3d829f1f0e0543b20f1900782d902d9
qin2500 <52422134+qin2500@users.noreply.github.com>
2026-04-06 17:40:28 -0400

    Updated Resume

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ const footer = () => {
     return(
         <div className="w-full">
             <div className="mt-[10%]  flex justify-center items-center font-bold">
-                <p className="text-slate-100">Last Updated July 2025</p>
+                <p className="text-slate-100">Last Updated February 2026</p>
             </div>
         </div>
     );
